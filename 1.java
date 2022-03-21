926fa5db7e16dbcd0f53076431d39e2a48a5d018
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-03-21 10:33:53 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,17 +1,28 @@
 import java.util.*;
-
-public class Hello{
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.print("Enter the name : ");
-        String name = sc.nextLine();
-        System.out.print("Enter the rollno : ");
-        int rollno = sc.nextInt();
-        System.out.print("Enter the mark : ");
-        int mark = sc.nextInt();
-        System.out.println("Name is : "+name);
-        System.out.println("Roll No is : "+rollno);
-        System.out.println("Mark is : "+mark);
-        sc.close();
-    }
+public class getString {
+	
+	String name;
+	int roll;
+	int mark;
+	public void get()
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the name");
+		name=s.nextLine();
+		System.out.println("enter the roll no");
+		roll=s.nextInt();
+		System.out.println("enter the mark");
+		mark=s.nextInt();	
+	}
+	public void display(){
+		
+	}
+	public static void main(String[] args) {
+		getString no=new getString();
+		no.get();
+		System.out.println("name="+no.name);
+		System.out.println("rollno="+no.roll);
+		System.out.println("mark="+no.mark);
+	}
 }
+
