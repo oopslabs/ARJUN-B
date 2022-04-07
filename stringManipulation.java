aa07e66cebf5e220c992435e99b4146a7e767dbc
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-04-07 16:02:11 +0530

    not completed

---
+++ stringManipulation.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class stringManipulation {
+
+public static void main(String args[]){
+    String s="java programming";
+    System.out.println("length of the string="+s.length());
+    String s2=" language";
+    String s3=s.concat(s2);
+    System.out.println("joined string is="+s3);
+
+}
+    
+}
