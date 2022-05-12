29a1bc3f7e6d0f24e61d2dbbe8d9d837a09edcf9
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-05-12 11:46:01 +0530

    updated

--- CO3/book.java
+++ CO3/book.java
@@ -23,36 +23,51 @@ class publisher extends book{
 	}
 }
  class fiction extends publisher{
-	 String genre;
+	 //String genre;
+	 String author;
 	 fiction(){
 		 super();
 		 Scanner s=new Scanner(System.in);
-		 System.out.print("enter the genre name\n");
-		 genre=s.next(); 
+//		 System.out.print("enter the genre name\n");
+//		 genre=s.next(); 
+		 System.out.print("enter the author name\n");
+		 author=s.next();
 	 }
+	 void display()
+		{
+			System.out.println("----THE FICTION BOOK DETAILS:-----");			
+			System.out.println("book name:"+name);
+			System.out.println("publisher:"+pname);
+			System.out.println("edition:"+edition);
+			System.out.println("author:"+author);
+			System.out.println("genere:"+"fiction");
+			System.out.println("price:"+price);
+		}
  }
- class literature extends fiction{
-	 String author;
+ class literature extends publisher{
+	 String autho;
 	 literature(){
 		 super();
 		 Scanner s=new Scanner(System.in);
 		 System.out.print("enter the author name\n");
-		 author=s.next();
+		 autho=s.next();
 	 }
 	 void display(){
-		 System.out.print("------BOOK DETAILS-----\n");
+		 System.out.print("------LITERATURE BOOK DETAILS-----\n");
 		 System.out.print("Name:"+name+"\n");
-		 System.out.print("Author:"+author+"\n");
+		 System.out.print("Author:"+autho+"\n");
 		 System.out.print("Price:"+price+"\n");
 		 System.out.print("Publisher:"+pname+"\n");
-		 System.out.print("Genre:"+genre+"\n");
+		 System.out.print("Genre:"+"Literature"+"\n");
 		 System.out.print("Edition:"+edition+"\n"); 
 	 } 
  }
 public class co3_4{
 	public static void main(String[] args) {
 		literature l=new literature();
+		fiction f=new fiction();
 		l.display();
+		f.display();
 
 	}
 }
