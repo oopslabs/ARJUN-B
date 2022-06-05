fd556dc0fbc055c771c6f2585255dc0dd5a60e79
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-06-05 21:49:38 +0530

    Update menu.java

--- CO3/menu.java
+++ CO3/menu.java
@@ -3,69 +3,68 @@ import java.io.*;
 
 public class menu {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int ch;
-		inter i;
-		circle c=new circle();
-		rectangle re=new rectangle();
-		do{
-		System.out.println("1.Area of Circle\n2.Perimeter of Circle\n3.Area of Rectangle\n4.Perimeter of Rectangle\nenter your choice\n");
-		ch=sc.nextInt();
-		switch(ch){
-		case 1:i=c;
-		i.area();
-		break;
-		case 2:i=c;
-		i.peri();
-		break;
-		case 3:i=re;
-		i.area();
-		break;
-		case 4:i=re;
-		i.peri();
-		break;
-		default:System.out.println("invalid choice\n");
-		}
-		}
-		while(ch!=0);
+		Scanner s=new Scanner(System.in);
+		int c;
+		do {
+			System.out.println("1.Find area of circle\n2.Find area of rectangle\n3.Exit");
+			c=s.nextInt();
+			switch(c) {
+			case 1: System.out.println("----Circle----");
+					circle obj1= new circle();
+					obj1.area();
+					obj1.perimeter();
+					break;
+			case 2: System.out.println("----Rectangle----");
+					rectangle obj2=new rectangle();
+					obj2.area();
+					obj2.perimeter();
+			case 3: System.out.println("Exiting....");
+					break;
+			default: System.out.println("Invalid");
+			}
+		}while(c!=3);		
+		
 	}
 }
 
 
 
-interface inter{
+
+interface Shape{
 	void area();
-	void peri();
+	void perimeter();
+
 }
 
-class circle implements inter{
-	Scanner sc=new Scanner(System.in);
-	public void area(){
-		System.out.println("enter the radius\n");
-		int r=sc.nextInt();
-		System.out.println("Area of Circle:"+3.14*r*r);
+class circle implements Shape{
+	int r;
+	circle(){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the radius");
+		r=s.nextInt();
 	}
-	public void peri(){
-		System.out.println("enter the radius\n");
-		int r=sc.nextInt();
-		System.out.println("Perimeter of Circle:"+2*3.14*r);
+	public void area(){
+		System.out.println("Area of Circle= "+(3.14*r*r));
+		
+	}
+	public void perimeter(){
+		System.out.println("Perimeter of Circle= "+(2*3.14*r));
+	}
+}
+
+class rectangle implements Shape{
+	int l,b;
+	rectangle(){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the l and b");
+		l=s.nextInt();
+		b=s.nextInt();
+	}
+	public void area(){
+		System.out.println("Area of Rectangle= "+(l*b));
 	}
 	
-}
-class rectangle implements inter{
-	Scanner sc=new Scanner(System.in);
-	public void area(){
-		System.out.println("enter the length\n");
-		int l=sc.nextInt();
-		System.out.println("enter the breadth\n");
-		int b=sc.nextInt();
-		System.out.println("Area of Rectangle:"+(l*b));
+	public void perimeter(){
+		System.out.println("Perimeter of Rectangle= "+(2*(l+b)));
 	}
-	public void peri(){
-		System.out.println("enter the length\n");
-		int l=sc.nextInt();
-		System.out.println("enter the breadth\n");
-		int b=sc.nextInt();
-		System.out.println("Perimeter of Rectangle:"+(2*(l+b)));
-	}	
 }
