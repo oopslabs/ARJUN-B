273c5c7417c3b869c53d9231e78513960cb223b7
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-06-10 12:10:23 +0530

    Create exception.java

---
+++ CO4/exception.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class NegativeNoException extends Exception{
+    public NegativeNoException(String s){
+        System.out.println(s);
+    }
+}
+
+public class negative {
+    public static void main(String args[]{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the limit\n");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            System.out.println("enter the number\n"+i);
+            int no=sc.nextInt();
+        }
+    })
+    
+}
+
