cff79be3a8e3c6f25605249f2e77ef4f6fb63a85
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-06-12 13:59:52 +0530

    co4-q4

--- CO4/exception.java
+++ CO4/exception.java
@@ -1,21 +1,32 @@
+
 import java.util.*;
 class NegativeNoException extends Exception{
     public NegativeNoException(String s){
         System.out.println(s);
     }
 }
-
 public class negative {
-    public static void main(String args[]{
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the limit\n");
-        int n=sc.nextInt();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            System.out.println("enter the number\n"+i);
-            int no=sc.nextInt();
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n,no,sum=0;
+    System.out.println("enter the limit\n");
+    n=sc.nextInt();
+    for (int i=1;i<=n;i++){
+        System.out.println("enter the number\n"+i);
+        no=sc.nextInt();
+        try{
+        if(no<0){
+            throw new NegativeNoException("negative!!!!");
         }
-    })
-    
+        else{
+            sum=sum+no;
+        }
+    }
+    catch(NegativeNoException e){
+        System.out.println(e);
+        i--;
+    }
+    }
+    System.out.println("average\n"+(sum/n));
+    }
 }
-
