6b8fdbc5635728262c61a6e098159473b8e1f39a
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-07-11 21:58:48 +0530

    co4-q11

---
+++ CO4/rmLinkedList.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+public class rmLinkedList {
+    public static void main(String args[])
+{
+	LinkedList<Integer> l=new LinkedList<Integer>();
+	l.add(12);
+	l.add(13);
+	l.add(44);
+	l.add(54);
+	System.out.println(l);
+	l.clear();
+	l.add(84);
+	System.out.println(l);
+}
+    
+}
