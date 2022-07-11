4afbe99770ee69cc016e74fd1f954193c88dd9c1
ARJUN B <71165245+Arjunb79@users.noreply.github.com>
2022-07-11 22:01:21 +0530

    co4-q13

---
+++ CO4/priorityQ.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class priorityQ {
+public static void main(String args[])
+{
+	int n;
+	String str;
+	PriorityQueue<String> pq=new PriorityQueue<String>();
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("enter the no of elements");
+	 n=sc.nextInt();
+	 System.out.println("enter elements");
+	 str=sc.nextLine();
+	 for(int i=0;i<n;i++)
+	 {
+		 str=sc.nextLine();
+		 pq.add(str);
+	 }
+	 System.out.println("peak "+pq.peek());
+	 System.out.println("poll "+pq.poll());
+	 Iterator i=pq.iterator();
+	 System.out.println("the strings are ");
+	 while(i.hasNext())
+	 {
+		 System.out.println(i.next()+" ");
+	 }
+}
+}
